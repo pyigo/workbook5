783c2a49718074dea08eb07ec7df6f283511589f
paulette <paulette.yigo@gmail.com>
2023-05-18 14:54:20 -0400

    added filtering option

--- dynamicImages/scripts/main.js
+++ dynamicImages/scripts/main.js
@@ -1,44 +1,61 @@
 //  window.onload = ()=>{
 //  Best way to delay content loaded is to use DOMContentLoaded instead of window onload
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", () => {
+
+    const tbody = document.querySelector("#list tbody")
 
     const year = document.getElementById("year")
     const image_list = document.getElementById("image_list")
     const output = document.getElementById("output")
     const clear = document.getElementById("clear")
 
-image_list.innerHTML = "<option>Make a Slection</option> "
-images.forEach(image=>{
-    let option =document.createElement("option");
-    option.innerHTML=image.title;
-    option.value = image.name;
-    image_list.appendChild(option);
-}); //END Images for each
+    image_list.innerHTML = "<option>Make a Slection</option> "
+    images.forEach(image => {
+        let option = document.createElement("option");
+        option.innerHTML = image.title;
+        option.value = image.name;
+        image_list.appendChild(option);
+    }); //END Images for each
 
-year.addEventListener("change", ()=>{
-    let selectedYear = year.selectedOptions[0].value;
-    alert(selectedYear);
-})
+    year.addEventListener("change", () => {
+        tbody.innerHTML = "";
+        let selectedYear = year.selectedOptions[0].value;
+        //alert(selectedYear);
+        images.forEach((image) => {
+            if (selectedYear == image.year) {
+                
+                let row = tbody.insertRow(-1) // end of rows
 
-image_list.addEventListener("change", e=>{
-    if(image_list.selectedIndex==0){ //IS FIRST ITEM "make a selection" e.g 0,or no items e.g -1
-        return; //DO NOTHING
-    }
-    let name=image_list.selectedOptions[0].value;
-    // alert(name);
-    let img= document.createElement("img");
-    img.src = "images/" + name;
-    output.appendChild(img);
-    // end on change image list
+                let cellYear = row.insertCell(0);
+                cellYear.innerHTML = image.year;
 
-})
+                let cellTitle = row.insertCell(1)
+                cellTitle.innerHTML = image.title;
 
-clear.addEventListener("click",()=> {
-output.innerHTML="";
-image_list.selectedIndex=0; //set select to first item, -1 for blank
+            }
 
-})// end clear button
+        })
+    }) //END YEAR CHANGE EVENT
 
-// window onload ends}
+    image_list.addEventListener("change", e => {
+        if (image_list.selectedIndex == 0) { //IS FIRST ITEM "make a selection" e.g 0,or no items e.g -1
+            return; //DO NOTHING
+        }
+        let name = image_list.selectedOptions[0].value;
+        // alert(name);
+        let img = document.createElement("img");
+        img.src = "images/" + name;
+        output.appendChild(img);
+        // end on change image list
+
+    })
+
+    clear.addEventListener("click", () => {
+        output.innerHTML = "";
+        image_list.selectedIndex = 0; //set select to first item, -1 for blank
+
+    })// end clear button
+
+    // window onload ends}
 
 }) //DOMContentLoaded ENDS
