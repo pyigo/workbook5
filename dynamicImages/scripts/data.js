5e213fbafdc57fe96fb5713f046b9753e58f934a
paulette <paulette.yigo@gmail.com>
2023-05-18 13:26:45 -0400

    created table and year options

--- dynamicImages/scripts/data.js
+++ dynamicImages/scripts/data.js
@@ -1,11 +1,11 @@
 const images = [
-    {name:"image1.jpg", title: "Fast and Furious"},                    
-    {name:"image2.jpg", title:"Transformers"}, 
-    {name:"image3.jpg", title:"Spider Man"},  
-    {name:"image4.jpg", title:"The Little Mermaid"}, 
-    {name:"image5.jpg", title:"The Guardians of Galaxy"},     
-    {name:"image6.jpg", title:"The Guardians of Galaxy"},
-    {name:"image7.jpg", title:"Mario Bros"},
-    {name:"image8.jpg", title:"Elemental"},
-    {name:"image9.jpg", title:"The Book Club"}
+    {year: 2023, name:"image1.jpg", title: "Fast and Furious"},                    
+    {year: 2023, name:"image2.jpg", title:"Transformers"}, 
+    {year: 2022, name:"image3.jpg", title:"Spider Man"},  
+    {year: 2023, name:"image4.jpg", title:"The Little Mermaid"}, 
+    {year: 2024, name:"image5.jpg", title:"The Guardians of Galaxy"},     
+    {year: 2023, name:"image6.jpg", title:"The Guardians of Galaxy"},
+    {year: 2022, name:"image7.jpg", title:"Mario Bros"},
+    {year: 2022, name:"image8.jpg", title:"Elemental"},
+    {year: 2024, name:"image9.jpg", title:"The Book Club"}
 ];
